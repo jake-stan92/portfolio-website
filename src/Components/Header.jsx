80323d4bba345ec13aad0f2ce1db15aef7717593
Jake <jake.stanier@hotmail.co.uk>
2023-09-18 02:44:01 -0400

    new logo added, new fonts added

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,6 +1,15 @@
-import logo from '../assets/logo.avif'
+// import logo from '../assets/logo.avif'
+import { useState } from 'react';
+import { useEffect } from 'react';
+import logoBlack from '../assets/JS Logo Black 1.png'
+import logoWhite from '../assets/JS Logo White 1.png'
 
 const Header = () => {
+
+    // let [logo, setLogo] = useState({
+    //     src: "",
+    //     alt: "",
+    // });
 
     const getHeaderHeight = () => {
         const height = document.querySelector('.header').offsetHeight;
@@ -19,11 +28,41 @@ const Header = () => {
         })
     }
 
+    const [isScrolled, setIsScrolled] = useState(false);
+
+    const listenScrollEvent = (e) => setIsScrolled(window.scrollY > 0);
+
+    useEffect(() => {
+        window.addEventListener('scroll', listenScrollEvent);
+    })
+
+    // const changeLogo = (e) => {
+    //     if (window.scrollY > 0) {
+    //         setLogo({
+    //             src: { logoBlack },
+    //             alt: "Company Logo",
+    //         });
+    //     } else {
+    //         setLogo({
+    //             src: { logoWhite },
+    //             alt: "Company Logo",
+    //         })
+    //     }
+    // }
+
+    // useEffect(() => {
+    //     window.addEventListener("scroll", changeLogo);
+    // }, []);
+
     return (
         <div className="header darkBG">
             <div className="container">
                 <div className="logo">
-                    <img id="header-logo" src={ logo } alt='logo'></img>
+                    {/* <img id="header-logo" src={`${logo.src}`} alt={ logo.alt } /> */}
+                    <img id="header-logo"
+                            src={isScrolled ? logoBlack: logoWhite}
+                            alt="Compant Logo"
+                        />
                 </div>
                 <div className="nav-links">
                     <ul className='links'>
