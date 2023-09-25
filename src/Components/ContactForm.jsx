f11ea0d75623d181b1fa55b737de901156e585af
Jake <jake.stanier@hotmail.co.uk>
2023-09-25 01:09:04 -0400

    netlify forms configured

--- src/Components/ContactForm.jsx
+++ src/Components/ContactForm.jsx
@@ -2,7 +2,7 @@ const ContactForm = () => {
     return (
         <div className="contact-form container">
                 <h3>CONTACT</h3>
-                <form id="contact-form" action="">
+                <form id="contact-form" name="contact-form" action="" method="POST" data-netlify="true">
                     <input name="first-name-input" type="text" placeholder="FIRST NAME *" required></input>
                     <input name="last-name-input" type="text" placeholder="LAST NAME *" required></input>
                     <input name="email-input" type="email" placeholder="EMAIL *" required></input>
